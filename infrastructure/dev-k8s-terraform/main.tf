4a182c9f2f09555af576fe07f6d91f158ca600d2
Fatih <mfaktasit@gmail.com>
2024-04-03 12:15:24 +0200

    code changes

--- infrastructure/dev-k8s-terraform/main.tf
+++ infrastructure/dev-k8s-terraform/main.tf
@@ -190,7 +190,7 @@ resource "aws_instance" "worker-2" {
     instance_type = "t3a.medium"
     vpc_security_group_ids = [aws_security_group.petclinic-kube-worker-sg.id, aws_security_group.petclinic-mutual-sg.id]
     key_name = "clarus"
-    subnet_id = "subnet-c41ba589"  # select own subnet_id of us-east-1a
+    subnet_id = "subnet-0b7c5cb8da69ae649"  # select own subnet_id of us-east-1a
     availability_zone = "us-east-1a"
     tags = {
         Name = "worker-2"
